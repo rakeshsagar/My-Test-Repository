be320f95dd11f9dec2eae74c1dca352cf37d6544
naveen09 <aechannaveen@gmail.com>
2015-02-10 14:18:07 +0530

    naveen checkin 3

--- NewMaven/src/main/java/rakesh/NewMaven/App.java
+++ NewMaven/src/main/java/rakesh/NewMaven/App.java
@@ -2,15 +2,12 @@ package rakesh.NewMaven;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Hello Naveen");
-        System.out.println("Hello Naveen 2");
-        System.out.println("Hello Rakesh 1");
+        System.out.println("Hello World!");
     }
 }
